2de43c05793377ffe7a6cddcd7096a77f142a8bd
Angel Vizuet <megustalafantabienfria@hotmail.com>
2019-08-12 12:04:19 -0500

    Env vars updated

--- webapp/src/app/constants/spotify/spotify.ts
+++
@@ -1,4 +0,0 @@
-export const CLIENT_ID = process.env.CLIENT_ID;
-export const CLIENT_SECRET = process.env.CLIENT_SECRET;
-export const REDIRECT_URI = process.env.REDIRECT_URI;
-export const SCOPES = process.env.SCOPES;
